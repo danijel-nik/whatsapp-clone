6cd3770e6fc6a8d9a896721e969e0e9806a53ed2
danijel_nik <danijel.nikolic@ymail.com>
2021-03-28 22:59:06 +0200

    Sidebar and Chat components

---
+++ components/Chat.js
@@ -0,0 +1,49 @@
+import { Avatar } from '@material-ui/core'
+import getRecipientEmail from '../utils/getRecipientEmail'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth, db } from '../firebase'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { useRouter } from 'next/router'
+import styled from 'styled-components'
+
+const Chat = ({ id, users }) => {
+    const router = useRouter()
+    const [user] = useAuthState(auth)
+    const [recipiantSnapshot] = useCollection(db.collection('users').where('email', '==', getRecipientEmail(users, user)))
+    const recipient = recipiantSnapshot?.docs?.[0]?.data()
+    const recipientEmail = getRecipientEmail(users, user)
+
+    const enterChat = () => {
+        router.push(`/chat/${id}`)
+    }
+
+    return (
+        <Container onClick={enterChat}>
+            {recipient ? (
+                <UserAvatar src={recipient?.photoURL} />
+            ): (
+                <UserAvatar>{recipientEmail[0].toUpperCase()}</UserAvatar>
+            )}
+            <p>{recipientEmail}</p>
+        </Container>
+    )
+}
+export default Chat
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    padding: 15px;
+    word-break: break-word;
+    transition: .3s ease;
+
+    :hover {
+        background-color: #e9eaeb;
+    }
+`
+
+const UserAvatar = styled(Avatar)`
+    margin: 5px;
+    margin-right: 15px;
+`
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,19 @@
-import { Avatar, IconButton, Button } from '@material-ui/core'
+import { Avatar, IconButton, Button, Tooltip } from '@material-ui/core'
 import ChatIcon from '@material-ui/icons/Chat'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 import SearchIcon from '@material-ui/icons/Search'
+import Chat from '../components/Chat'
 import * as EmailValidator from 'email-validator'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { auth, db } from '../firebase'
 import styled from 'styled-components'
 
 const Sidebar = () => {
+    const [user] = useAuthState(auth)
+    const userChatRef = db.collection('chats').where('users', 'array-contains', user.email)
+    const [chatsSnapshot] = useCollection(userChatRef)
+
     const createChat = () => {
         const input = prompt(
             "Please enter an email address for the user you wish to chat with"
@@ -13,15 +21,32 @@ const Sidebar = () => {
 
         if (!input) return null
 
-        if (EmailValidator.validate(input)) {
-            // need to add the chat into the DB 'chats' collection
+        if (EmailValidator.validate(input) && !chatAlreadyExists(input) && input !== user.email) {
+            // add the chat into the DB 'chats' collection if it is valid and it doesn't already exist
+            db.collection('chats').add({
+                users: [user.email, input]
+            })
         }
     }
+
+    // !! converts result to boolean
+    // ?  can be undefined
+    const chatAlreadyExists = (recipientEmail) => (
+        !!chatsSnapshot?.docs.find(
+            (chat) => 
+                chat.data().users.find(
+                    (user) => 
+                        user === recipientEmail
+            )?.length > 0
+        )
+    )
 
     return (
         <Container>
             <Header>
-                <UserAvatar />
+                <Tooltip title={`${user.email} | Sign Out`}>
+                    <UserAvatar src={user.photoURL} onClick={() => auth.signOut()} />
+                </Tooltip>
 
                 <IconsContainer>
                     <IconButton>
@@ -43,6 +68,7 @@ const Sidebar = () => {
             </SidebarButton>
 
             {/** List of chats */}
+            {chatsSnapshot?.docs.map(chat => <Chat key={chat.id} id={chat.id} users={chat.data().users} />)}
         </Container>
     )
 }
