1f23551a80e39c6e95ae3bb530e9be3d53262ce7
danijel_nik <danijel.nikolic@ymail.com>
2021-03-30 23:14:55 +0200

    chat/id page

---
+++ components/ChatScreen.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+const ChatScreen = () => {
+    return (
+        <Container>
+            <h1>This is a chat</h1>
+        </Container>
+    )
+}
+
+export default ChatScreen
+
+const Container = styled.div`
+
+`
