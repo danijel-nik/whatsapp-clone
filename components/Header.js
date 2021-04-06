025351d82694133e77530eea03a8de4ebd0c4020
danijel_nik <danijel.nikolic@ymail.com>
2021-04-06 20:48:23 +0200

    whatsapp icon

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 
 const Header = ({ title }) => {
-    const favicon = "http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png";
+    const favicon = "/whatsapp.png";
     return (
         <Head>
             <title>{title}</title>
